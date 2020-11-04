6f66b99e5d1edca9f3361a7cd1fbb4bb445dcdc7
afkpuezo <andrewfcurry@gmail.com>
2020-11-04 17:31:33 -0500

    wrote the Request class

---
+++ src/main/java/Request.java
@@ -0,0 +1,49 @@
+import java.util.List;
+
+/**
+ * This class represents a single requested action/input/change from a user.
+ * 
+ * @author Andrew Curry
+ *
+ */
+public class Request {
+
+	// enum(s)
+	public enum RequestType{
+		REGISTER_USER, 
+		LOG_IN, 
+		APPLY_OPEN_ACCOUNT, 
+		APPROVE_OPEN_ACCOUNT,
+		DENY_OPEN_ACCOUNT,
+		CLOSE_ACCOUNT,
+		ADD_ACCOUNT_OWNER,
+		REMOVE_ACCOUNT_OWNER,
+		DEPOSIT,
+		WITHDRAW,
+		TRANSFER
+	}
+	
+	// instance variables
+	private RequestType type;
+	private List<String> params; // order WILl matter for this
+	
+	
+	// constructor(s)
+	
+	
+	public Request (RequestType type, List<String> params) {
+		this.type = type;
+		this.params = params;
+	}
+	
+	
+	// getters
+	
+	public RequestType getType() {
+		return this.type;
+	}
+	
+	public List<String> getParams() {
+		return params;
+	}
+}

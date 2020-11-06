9d2399d519ed5466c1963f4a3b6b7433743c5636
afkpuezo <andrewfcurry@gmail.com>
2020-11-06 15:20:41 -0500

    tested denying account applications

--- src/test/java/bankTest/TestBankSystem.java
+++ src/test/java/bankTest/TestBankSystem.java
@@ -304,7 +304,7 @@ public class TestBankSystem {
 	}
 	
 	@Test
-	public void handleApprove() throws BankDAOException{
+	public void handleApproveAccount() throws BankDAOException{
 		
 		logInHelp("user", "pass");
 		
@@ -360,6 +360,73 @@ public class TestBankSystem {
 		params.add("444");
 		request = new Request(
 				RequestType.APPROVE_OPEN_ACCOUNT,
+				params);
+		mio.setNextRequest(request);
+		bank.testLoop();
+		
+		output = mio.getCachedOutput();
+		assertEquals(
+				BankSystem.BANK_ACCOUNT_NOT_PENDING_MESSAGE, 
+				output.get(output.size() - 1));
+	}
+	
+	@Test
+	public void handleDenyAccount() throws BankDAOException {
+		
+		logInHelp("user", "pass");
+		
+		Request request = new Request(RequestType.APPLY_OPEN_ACCOUNT);
+		mio.setNextRequest(request);
+		bank.testLoop();
+		
+		request = new Request(
+				RequestType.LOG_OUT,
+				new ArrayList<>());
+		mio.setNextRequest(request);
+		bank.testLoop();
+		
+		UserProfile up = tdao.readUserProfile("user");
+		int accID = up.getOwnedAccounts().get(1); // new one should be last
+		
+		logInHelp("admin", "admin");
+		List<String> params = new ArrayList<String>();
+		params.add("" + accID);
+		request = new Request(
+				RequestType.DENY_OPEN_ACCOUNT,
+				params);
+		mio.setNextRequest(request);
+		bank.testLoop();
+		
+		// is the account actually approved?
+		List<Object> output = mio.getCachedOutput();
+		assertEquals(
+				BankSystem.ACCOUNT_DENIED_MESSAGE, 
+				output.get(output.size() - 1));
+		BankAccount ba = tdao.readBankAccount(accID);
+		assertEquals(BankAccountStatus.CLOSED, ba.getStatus());
+	}
+	
+	@Test
+	public void handleDenyBadAccount() throws BankDAOException {
+		
+		logInHelp("admin", "admin");
+		List<String> params = new ArrayList<String>();
+		params.add("3234259");
+		Request request = new Request(
+				RequestType.DENY_OPEN_ACCOUNT,
+				params);
+		mio.setNextRequest(request);
+		bank.testLoop();
+		
+		List<Object> output = mio.getCachedOutput();
+		assertEquals(
+				BankSystem.BANK_ACCOUNT_DOES_NOT_EXIST_PREFIX + "3234259", 
+				output.get(output.size() - 1));
+		
+		params = new ArrayList<String>();
+		params.add("444");
+		request = new Request(
+				RequestType.DENY_OPEN_ACCOUNT,
 				params);
 		mio.setNextRequest(request);
 		bank.testLoop();
